6d6f9f7187a72e54239ae95396ed039499c9fbca
aaron <aaronxyliu@gmail.com>
2023-01-28 01:48:20 -0500

    update

---
+++ script/crawler.py
@@ -0,0 +1,42 @@
+from urllib.request import urlopen
+import json
+
+
+# cdnjs API manual: https://cdnjs.com/api
+
+# First, get all library list
+res1 = urlopen('https://api.cdnjs.com/libraries')
+all_libs = json.loads(res1.read())
+
+print(f'''Total library: {all_libs['available']}''')
+
+for lib_info in all_libs['results']:
+    lib_name = lib_info['name']
+    # Get library's all versions
+    res2 = urlopen(f'https://api.cdnjs.com/libraries/{lib_name}?fields=versions')
+    version_list = json.loads(res2.read())['versions']
+
+
+    files_dict = {}
+    for version in version_list:
+        if version[0] == '0':
+            # Skip experimental versions
+            continue
+        
+        files_dict[version] = []
+        res3 = urlopen(f'https://api.cdnjs.com/libraries/{lib_name}/{version}?fields=files')
+        file_list = json.loads(res3.read())['files']
+
+        for file in file_list:
+            # Only consider minified js files
+            if file[-6:] == 'min.js' or file[-4:] == '.mjs':
+                files_dict[version].append(file)
+    
+    json_str = json.dumps(files_dict)
+
+
+
+
+# res = urlopen('https://api.cdnjs.com/libraries/vue/2.6.11?fields=files')
+
+# print(json.loads(res.read()))
