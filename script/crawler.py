21cdb31843183753c43bede904e08a57edf1cc4d
Xinyue Liu <xinyueliu@Xinyues-MacBook-Air.local>
2023-01-28 01:52:51 -0500

    add database connection info

--- script/crawler.py
+++ script/crawler.py
@@ -1,6 +1,19 @@
 from urllib.request import urlopen
 import json
+from dotenv import load_dotenv
+load_dotenv()
+import MySQLdb
 
+connection = MySQLdb.connect(
+  host= 'us-east.connect.psdb.cloud',
+  user='yain51suytl8vm1cm4b2',
+  passwd= 'pscale_pw_pEiVDtydTJqIpuJ68NRKenF0ncp6jjYipJkfxFDIHDN',
+  db= 'js-lib-detect-trees',
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
 
 # cdnjs API manual: https://cdnjs.com/api
 
